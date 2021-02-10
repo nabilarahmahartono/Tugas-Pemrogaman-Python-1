641da04def907ff0df67162109869e7543fdc995
nabilarahmahartono <78465098+nabilarahmahartono@users.noreply.github.com>
2021-02-10 14:10:27 +0700

    Add files via upload

---
+++ program 10.py
@@ -0,0 +1,8 @@
+celcius = float(input('Ketik Nilai Celcius : '))
+reamur = 4/5*celcius
+fahrenheit = 9/5*celcius+32
+print("celcius = ",celcius)
+print("reamur = ",reamur)
+print("fahrenheit =",fahrenheit)
+                    
+                    
---
+++ test.py
@@ -0,0 +1,47 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+== RESTART: C:/Users/user/AppData/Local/Programs/Python/Python39/program 1.py ==
+input valid kilometer : 21
+21.00 kilometers is equal to 13.05
+>>> 
+== RESTART: C:/Users/user/AppData/Local/Programs/Python/Python39/program 2.py ==
+num 1 : 2
+num 2 : 3
+penjumlahan 2.0 dan 3.0 adalah 5.0
+>>> 
+== RESTART: C:/Users/user/AppData/Local/Programs/Python/Python39/program 3.py ==
+nilai x sekarang : 10
+nilai y sekarang : 5
+>>> 
+== RESTART: C:/Users/user/AppData/Local/Programs/Python/Python39/program 4.py ==
+akar dari  9  adalah 3.0
+>>> 
+== RESTART: C:/Users/user/AppData/Local/Programs/Python/Python39/program 5.py ==
+masukkan tahun : 2020
+2020 adalah tahun kabisat
+>>> 
+======================================== RESTART: C:\Users\user\AppData\Local\Programs\Python\Python39\program 5.py ========================================
+masukkan tahun : 2017
+2017 bukan tahun kabisat
+>>> 
+== RESTART: C:/Users/user/AppData/Local/Programs/Python/Python39/program 6.py ==
+nilai terbesar : 14
+>>> 
+== RESTART: C:/Users/user/AppData/Local/Programs/Python/Python39/program 7.py ==
+Input nilai : 17
+17 adalah ganjil
+>>> 
+== RESTART: C:/Users/user/AppData/Local/Programs/Python/Python39/program 8.py ==
+FPB dari  106 dan 24 = 2
+>>> 
+== RESTART: C:/Users/user/AppData/Local/Programs/Python/Python39/program 9.py ==
+bilangan : 5
+faktorial dari 5 adalah 120
+>>> 
+== RESTART: C:/Users/user/AppData/Local/Programs/Python/Python39/program 10.py =
+Ketik Nilai Celcius : 180
+celcius =  180.0
+reamur =  144.0
+fahrenheit = 356.0
+>>> 
